01ba066aa4886b081d5934d0575197dcb9137a3a
erickmwei680@gmail.com <erickmwei680@gmail.com>
2024-06-24 20:02:38 +0300

    added some css

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaXTwitter } from "react-icons/fa6";
 import './Footer.css';
 import footer_logo from '../../assets/MyLogo.png';
 import user_icon from '../../assets/user_icon.svg';
@@ -29,9 +31,22 @@ const Footer = () => {
                     </div>
                 </div> */}
             </div>
+            <div className="footer-icons">
+                <a href="https://x.com/Ricu235419" target="_blank" rel="noopener noreferrer">
+                    <FaXTwitter size={24} />
+                </a>
+                <a href="https://github.com/Erick-dev98" target="_blank" rel="noopener noreferrer">
+                    <FaGithub size={24} />
+                </a>
+                <a href="https://www.linkedin.com/in/erick-mutua-47000929a" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin size={24} />
+                </a>
+            </div>
             <hr />
-            <div className="footer-botttom">
-                <p className="footer-bottom-left">&copy; {new Date().getFullYear()} All rights reserved. <br/> Made by Erick Mutua.</p>
+            <div className="footer-bottom">
+                <div className="footer-bottom-left">
+                    <p>&copy; {new Date().getFullYear()} All rights reserved. <br/> Made by Erick Mutua.</p>
+                </div>
                 {/* <div className="footer-bottom-right">
                     <p>Term of Services</p>
                     <p>Privacy Policy</p>
